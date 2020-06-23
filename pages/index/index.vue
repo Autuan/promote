7bba5b4eb83593e4dfcd168bbbb388e90351282d
autuan.yu <autuan.yu@gmail.com>
2020-06-23 17:54:56 +0800

    新建二维码展示页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -20,8 +20,7 @@
 
         <swiper class="card-swiper round-dot" :indicator-dots="true" :circular="true" :autoplay="true" interval="5000"
             duration="500" @change="cardSwiper" indicator-color="#8799a3" indicator-active-color="#0081ff">
-            <swiper-item v-for="(item,index) in images" :key="index" :class="cardCur==index?'cur':''"
-            @tap="previewImg(item.image)">
+            <swiper-item v-for="(item,index) in images" :key="index" :class="cardCur==index?'cur':''" @tap="previewImg(item.image)">
                 <view class="swiper-item">
                     <image :src="item.image" mode="aspectFit"></image>
                     <!-- <video :src="item.url" autoplay loop muted :show-play-btn="false" :controls="false" objectFit="cover"
@@ -33,40 +32,39 @@
 
 
         <view class="cu-card article no-card">
-            <view class="cu-item shadow" v-for="i in 8" :key="i">
-                <view class="title">
-                    <view class="text-cut">标题标题标题标题标题标题标题标题标题标题</view>
+            <view class="cu-item shadow" v-for="task in tasks" :key="task.id">
+                <view class="title"  @tap="toArticleDetail(task.articleId)">
+                    <view class="text-cut">{{task.name}}</view>
                 </view>
                 <view class="content">
-                    <image src="http://localhost/profile/upload/2020/06/22/d05a37f530c8c34b9c06ac7bcd342894.jpg" mode="aspectFill"></image>
-                    
+                    <image :src="task.image" mode="aspectFill"  @tap="toArticleDetail(task.articleId)"></image>
+
                     <view class="desc">
-                        <view>
-                        	<view class="cu-tag bg-red light ">正义天使</view>
-                        	<view class="cu-tag bg-green light ">史诗</view>
-                        	<view class="cu-tag bg-green light ">史诗</view>
+                        <view  @tap="toArticleDetail(task.articleId)">
+                            <view class="cu-tag bg-red light " :key="i" v-for="(tag,i) in getTagList(task.tags)">{{tag}}</view>
                         </view>
-                        <view class="text-content"> 
-                        <text>内容内容内容内容内容内容内容</text>
+                        <view class="text-content margin-top">
+                            <!-- <text class="response">点击查看推广攻略</text> -->
+                            <!-- <text>&nbsp;</text> -->
                             <button class="cu-btn line-orange">二维码</button>
-                            <button class="cu-btn bg-gray margin-left" >推广赚80元</button>
+                            <button class="cu-btn bg-gray margin-left"  @tap="toArticleDetail(task.articleId)">推广赚{{task.reward}}元</button>
                         </view>
                         <view>
-                        	
+
                         </view>
                     </view>
                 </view>
             </view>
-            
-            
+
+
         </view>
 
-    <hx-preview-img :list.sync="previewImgList" :current.sync="currentImg" :start.sync="PINum"></hx-preview-img>
+        <hx-preview-img :list.sync="previewImgList" :current.sync="currentImg" :start.sync="PINum"></hx-preview-img>
     </view>
 </template>
 
 <script>
-      import hxPreviewImg from "@/components/hx-preview-img/hx-preview-img.vue";
+    import hxPreviewImg from "@/components/hx-preview-img/hx-preview-img.vue";
     export default {
         components: {
             hxPreviewImg,
@@ -74,32 +72,38 @@
         },
         data() {
             return {
-               currentImg: '',
-               previewImgList: [],
-               PINum: 0,
-                 articles:[],
-                 images:[],
+                currentImg: '',
+                previewImgList: [],
+                PINum: 0,
+                articles: [],
+                images: [],
+                tasks: [],
                 cardCur: 0,
             }
         },
         onLoad() {
             // console.info(this.swiperList.map(item=>item.url))
-			let page = this;
-			// 请求数据
-			getApp().request({
-			    url: '/front/index/info',
-			    successParse: function(data) {
-			        console.info(data)
-			        // uni.setStorageSync('member', data)
-			// 文章轮播
-			page.articles = data.articles;
-			// 图片轮播
-			page.images = data.images;
-            page.previewImgList = page.images.map(item=>item.image);
-			    }
-			})
+            let page = this;
+            // 请求数据
+            getApp().request({
+                url: '/front/index/info',
+                successParse: function(data) {
+                    console.info(data)
+                    // uni.setStorageSync('member', data)
+                    // 文章轮播
+                    page.articles = data.articles;
+                    // 图片轮播
+                    page.images = data.images;
+                    page.previewImgList = page.images.map(item => item.image);
+                    // 任务
+                    page.tasks = data.tasks;
+                }
+            })
         },
         methods: {
+            getTagList(tags){
+              return tags.split(',');
+            },
             previewImg(imgSrc) {
                 this.currentImg = imgSrc;
                 // this.previewImgList = this.swiperList.map(item=>item.url);
@@ -108,10 +112,13 @@
             cardSwiper(e) {
                 this.cardCur = e.detail.current
             },
-            toArticleDetail(id){
+            toArticleDetail(id) {
+                if(id) {
+                    
                 uni.navigateTo({
-                    url:'/pages/article/article?articleId='+id
+                    url: '/pages/article/article?articleId=' + id
                 })
+                }
             }
         }
     }
---
+++ pages/qrcode/code.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+        <view>二维码</view>
+		<image src="http://localhost/front/task/get" mode="aspectFill" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src:'',
+			}
+		},
+        onLoad(option) {
+            
+        },
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
